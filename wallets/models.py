a938207cce6c7a68009b841871d9f0a7cbdea9f5
Wieslaw Ruszowski <wieslaw.ruszowski@virtualstock.co.uk>
2018-12-11 16:38:24 +0100

    wallet model has been added

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+"""
+Wallet Models for User
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+UserModel = get_user_model()
+
+
+class WalletModel(models.Model):
+    """
+    Wallet Model class for User
+    """
+    WALLET_TYPE = (
+        {'R', 'Real Money EUR'},
+        {'B', 'Bonus Money BNS'},
+    )
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    wallet_type = models.CharField(max_length=1, choices=WALLET_TYPE)
+    value = models.PositiveSmallIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name = 'Wallet'
+        verbose_name_plural = 'Wallets'
+
+    def __str__(self):
+        return self.user
