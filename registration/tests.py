81dc4d507e619e6e39feb6ad7e3a2bcf871924e1
Wieslaw Ruszowski <wieslaw.ruszowski@virtualstock.co.uk>
2018-12-11 22:09:21 +0100

    tests for bonuses

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 
-# Create your tests here.
+from .models import BonusForLoginModel, BonusForDepositModel
+
+UserModel = get_user_model()
+
+
+class WalletBonusModelClassTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserModel.objects.create_user(username='John', password='secret')
+
+    def test_adding_bonus_after_login(self):
+        _client = Client()
+        _client.login(username='John', password='secret')
+        self.assertTrue(BonusForLoginModel.objects.get(user=self.user))
+        self.assertTrue(BonusForDepositModel.objects.get(user=self.user))
